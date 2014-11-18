2a402a36f54622c1fa72356c7b2299384273df40
Alex Mischur <psychomuzi@users.sourceforge.net>
2014-11-18 22:22:25 +0100

    added one more testcase removed guava dependency because of bug https://bugs.eclipse.org/bugs/show_bug.cgi?id=427862

--- GuavaEclipseSwtBotTest/src/main/java/net/sf/guavaeclipse/swtbot/AbstractSwtBotIntegrationTest.java
+++ GuavaEclipseSwtBotTest/src/main/java/net/sf/guavaeclipse/swtbot/AbstractSwtBotIntegrationTest.java
@@ -1,9 +1,28 @@
+/* Copyright 2014
+ * 
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.sf.guavaeclipse.swtbot;
 
 import static org.hamcrest.CoreMatchers.is;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.URL;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
 
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
 import org.eclipse.swtbot.eclipse.finder.waits.Conditions;
@@ -11,9 +30,6 @@ import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
 import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-
-import com.google.common.base.Charsets;
-import com.google.common.io.Resources;
 
 public abstract class AbstractSwtBotIntegrationTest {
 
@@ -72,13 +88,22 @@ public abstract class AbstractSwtBotIntegrationTest {
 		}
 	}
 
-	public String readExpectedFile(String fileName) {
-		URL url = this.getClass().getClassLoader().getResource(fileName);
+	public String readExpectedFile(String fileName) throws IOException,
+			URISyntaxException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(this
+				.getClass().getClassLoader().getResourceAsStream(fileName)));
 		try {
-			return Resources.toString(url, Charsets.UTF_8).replaceAll("\r", "");
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
+			StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
+
+			while (line != null) {
+				sb.append(line);
+				sb.append("\n");
+				line = br.readLine();
+			}
+			return sb.toString();
+		} finally {
+			br.close();
 		}
 	}
 
--- GuavaEclipseSwtBotTest/src/main/java/net/sf/guavaeclipse/swtbot/EqualsHashCodeIntegrationTest.java
+++ GuavaEclipseSwtBotTest/src/main/java/net/sf/guavaeclipse/swtbot/EqualsHashCodeIntegrationTest.java
@@ -1,3 +1,20 @@
+/* Copyright 2014
+ * 
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.sf.guavaeclipse.swtbot;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -31,13 +48,11 @@ public class EqualsHashCodeIntegrationTest extends
 		        "package net.sf.guavaeclipse.test;\n"
 		      + "\n" 
 		      + "public class SampleSimple {\n" 
-		      + "	\n" 
-		      + "	\n" 
+		      + "\n" 
 		      + "	private int intValue;\n" 
-		      + "	\n"
+		      + "\n"
 		      + "	private String strValue;\n" 
-		      + "	\n" 
-		      + "	\n" 
+		      + "\n" 
 		      + "}\n");
         cutEditor.save();
 		cutEditor.selectLine(9);
@@ -71,10 +86,10 @@ public class EqualsHashCodeIntegrationTest extends
                 "package net.sf.guavaeclipse.test;\n"
               + "\n" 
               + "public class ExtendSimpleSample extends SampleSimple {\n" 
-              + "   \n" 
-              + "   \n" 
-              + "   private Object objectValue;\n" 
-              + "   \n"
+              + "\n" 
+              + "\n" 
+              + "	private Object objectValue;\n" 
+              + "\n"
               + "}\n");
         cutEditor.save();
         cutEditor.selectLine(7);
@@ -105,7 +120,7 @@ public class EqualsHashCodeIntegrationTest extends
 				"\n" + 
 				"public interface InterfaceSample {\n" + 
 				"\n" + 
-				"	public String getStringValue\n" + 
+				"	public String getStringValue();\n" + 
 				"}\n");
 		cutEditor.save();
 
@@ -126,14 +141,14 @@ public class EqualsHashCodeIntegrationTest extends
 				"package net.sf.guavaeclipse.test;\n" + 
 				"\n" + 
 				"public class SampleImplementsInterface implements InterfaceSample {\n" + 
-				"   \n" + 
-				"   private String stringValue;\n" + 
-				"   \n" + 
-				"   @Override\n" +
-				"   public String getStringValue() {\n" +
-				"   	return this.stringValue;\n" +
-				"   }\n" +
-				"   \n" + 
+				"\n" + 
+				"	private String stringValue;\n" + 
+				"\n" + 
+				"	@Override\n" +
+				"	public String getStringValue() {\n" +
+				"		return this.stringValue;\n" +
+				"	}\n" +
+				"\n" + 
 				"}\n");
 		cutEditor.save();
 		cutEditor.selectLine(11);
@@ -147,6 +162,51 @@ public class EqualsHashCodeIntegrationTest extends
 
 		String editorText = cutEditor.getText();
 		String expectedText = readExpectedFile("Expected_EqualsHashCodeForInterfaceClass.txt");
+		assertThat(editorText, is(expectedText));
+	}
+
+	@Test
+	public void createEqualsHashCodeForInterfaceClassAndExtendedClass()
+			throws Exception {
+		bot.tree().getTreeItem("SampleJavaProject").select();
+		bot.menu("New").menu("Class").click();
+		bot.textWithLabel("Pac&kage:").setText("net.sf.guavaeclipse.test");
+		bot.textWithLabel("Na&me:").setText("SampleExtendedAndInterface");
+		bot.textWithLabel("&Superclass:").setText("net.sf.guavaeclipse.test.SampleSimple");
+		bot.button("Add...").click();
+		bot.text().setText("net.sf.guavaeclipse.test.InterfaceSample");
+		sleep();
+		bot.button("OK").click();
+		// sleep();
+		bot.button("Finish").click();
+		sleep();
+		
+		SWTBotEclipseEditor cutEditor = bot.activeEditor().toTextEditor();
+		cutEditor
+				.setText("package net.sf.guavaeclipse.test;\n"
+						+ "\n"
+						+ "public class SampleImplementsInterface extends SampleSimple implements InterfaceSample {\n"
+						+ "\n" 
+						+ "	private String stringValue;\n"
+						+ "\n" 
+						+ "	@Override\n"
+						+ "	public String getStringValue() {\n"
+						+ "		return this.stringValue;\n" 
+						+ "	}\n" 
+						+ "\n"
+						+ "}\n");
+		cutEditor.save();
+		cutEditor.selectLine(11);
+		SWTBotMenu contextMenu = cutEditor.contextMenu("Google Guava Helper");
+		contextMenu.setFocus();
+		contextMenu.menu("Generate hashCode() and equals()").click();
+		bot.button("Select All").click();
+		bot.button("OK").click();
+		sleep();
+		cutEditor.save();
+
+		String editorText = cutEditor.getText();
+		String expectedText = readExpectedFile("Expected_EqualsHashCodeForInterfaceAndExtendedClass.txt");
 		assertThat(editorText, is(expectedText));
 	}
 
